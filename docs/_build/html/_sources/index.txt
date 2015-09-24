39812a817a6f4a1dd641b55509305ac1a9a36408
Klinisk genetik <data_in@v01s979.vll.se>
2015-09-24 10:50:20 +0200

    documentation changes

--- docs/_build/html/_sources/index.txt
+++ docs/_build/html/_sources/index.txt
@@ -1,9 +1,9 @@
 .. calls2xls documentation master file, created by
-   sphinx-quickstart on Wed Sep 23 14:20:24 2015.
+   sphinx-quickstart on Thu Sep 24 10:49:07 2015.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Calls2xls Documentation!
+Welcome to calls2xls's documentation!
 =====================================
 
 Contents:
--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Calls2xls Documentation!"],titleterms:{calls2xl:0,document:0,indic:0,tabl:0,welcom:[]}})
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to calls2xls&#8217;s documentation!"],titleterms:{calls2xl:0,document:0,indic:0,tabl:0,welcom:0}})
